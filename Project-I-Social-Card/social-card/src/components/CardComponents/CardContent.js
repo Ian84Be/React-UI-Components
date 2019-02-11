023f27816b7b3b7a77e10e42bafc356517bed54f
Ian Be <ian84be@gmail.com>
2019-02-11 16:25:29 -0600

    buildout social card w/ minimal styling

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+
+function CardContent() {
+    return (
+        <div className="card-content">
+            <CardBanner />
+            <div className="card-text">
+                <h2>Get started with React</h2>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cumque molestias asperiores, hic quos perferendis eius repellendus esse blanditiis ducimus est rem aut fugit iusto dolorem voluptate. Assumenda veniam reiciendis qui.</p>
+                <a href="https://www.reactjs.org" target="_blank" rel="noopener noreferrer">reactjs.org</a>
+            </div>
+        </div>
+    )
+}
+export default CardContent;
--- Project-I-Social-Card/social-card/src/index.js
+++ Project-I-Social-Card/social-card/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import HeaderContainer from './components/HeaderComponents/HeaderContainer';
+import CardContainer from './components/CardComponents/CardContainer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function SocialCard() {
+    return (
+        <div className="social-card">
+        <HeaderContainer />
+        <CardContainer />
+        </div>
+    )
+}
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<HeaderContainer />, document.getElementById('root'));
+ReactDOM.render(<SocialCard />, document.getElementById('root'));
