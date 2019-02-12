83594adfc8240e229a76ff1dcf443b960261131f
Ian Be <ian84be@gmail.com>
2019-02-11 18:00:53 -0600

    progress at stand-up

--- Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ActionButton.js
@@ -1,2 +1,11 @@
 import React from 'react';
 import './Button.css';
+
+class ActionButton extends React.Component {
+    render() {
+        return (
+            <button className={props.buttonStyle}>{props.text}</button>
+        );
+    }
+}
+export default ActionButton;
